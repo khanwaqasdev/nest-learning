20e7505590b96e8a33f2c61ea2e44b43cc381a26
Waqas Khan <khanwaqas.dev@gmail.com>
2024-01-26 02:37:35 +0500

    typeorm and config module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+  envFilePath:"local.env"
+  }),TypeOrmModule.forRootAsync({
+    imports:[ConfigModule],
+    inject:[ConfigService],
+    useFactory:(config:ConfigService)=>{
+      return {
+        type:'mysql',
+        host:config.get('DB_HOST'),
+        port:config.get('DB_HOST'),
+        username:config.get('DB_USER'),
+        password:config.get('DB_PASSWORD'),
+        database:config.get('DB_DATABASE'),
+        entities:[],
+        autoLoadEntities:true,
+        synchronize:true
+      }
+    },
+    dataSourceFactory: async (options) => {
+      const dataSource = await new DataSource(options).initialize();
+      return dataSource;
+    },
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
